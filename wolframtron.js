c2819b35447e0a6fb6f747c637408ce705068ebd
Will Ricketts <will@willricketts.com>
2013-12-31 15:51:44 -0500

    added testing stuff

--- wolframtron.js
+++ wolframtron.js
@@ -9,15 +9,23 @@ var T = new Twit({
   , access_token_secret:  ''
 })
 
-function wolframtron() {
-	var stream = T.stream('user');
+function wolframBot() {
+  var stream = T.stream('user');
       stream.on('tweet', function (tweet){
-        if (tweet.in_reply_to_screen_name == '')
+        if (tweet.in_reply_to_screen_name == 'Wolframtron')
         {
-          console.log('Mention:  ' + tweet.text);
-          var question = tweet.text.replace("", "");
+          console.log('Mention:  ' + tweet.user.screen_name + " -- " + tweet.text);
+          //var question = tweet.text.replace("@wolframtron", "")
+          var reply = "Well hello there, " + "@" + tweet.user.screen_name;
+          console.log("reply: " + reply);
+
+          T.post('statuses/update', { status: reply }, function(err, reply) {
+            console.log("");
+            console.log("Tweet successfully posted!")
+            console.log("");
+          })
         }
       })
 }
 
-wolframtron();
+wolframBot();
